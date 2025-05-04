16088ced053b055a0f986bc9b7ac23e9d22859c0
Karol <karolddelgado@unicauca.edu.co>
2025-05-04 17:16:47 -0600

    actualizaciones de empresa

--- Empresa-microService/src/main/java/com/academic_projects_prototype/companyMicroservice/repositories/CompanyRepository.java
+++ Empresa-microService/src/main/java/com/academic_projects_prototype/companyMicroservice/repositories/CompanyRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    // para eliminar
+    // en ICompanyService está
     Optional<Company> findByNit(Long nit);
 }
