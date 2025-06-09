7f23c410d458e7d440ac0dd5ed3098d02a4d1114
Anvinov <anvigi2001@gmail.com>
2025-06-08 20:30:43 -0500

    Fix 'client duplicated' in student-service

--- StudentService/src/main/java/co/edu/unicauca/studentservice/controller/StudentApplicationController.java
+++ StudentService/src/main/java/co/edu/unicauca/studentservice/controller/StudentApplicationController.java
@@ -1,31 +1,27 @@
 package co.edu.unicauca.studentservice.controller;
 
-import co.edu.unicauca.studentservice.infra.client.ProjectApplicationRequestClient;
-import co.edu.unicauca.studentservice.infra.dto.ProjectApplicationResponseDTO;
+import co.edu.unicauca.studentservice.infra.client.CoordinatorClient;
 import co.edu.unicauca.studentservice.service.StudentApplicationService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/student/application")
 public class StudentApplicationController {
 
     private final StudentApplicationService studentApplicationService;
-    private final ProjectApplicationRequestClient projectApplicationRequestClient;
+    private final CoordinatorClient coordinatorClient;
 
-    public StudentApplicationController(StudentApplicationService studentApplicationService, ProjectApplicationRequestClient projectApplicationRequestClient) {
+    public StudentApplicationController(StudentApplicationService studentApplicationService, CoordinatorClient coordinatorClient) {
         this.studentApplicationService = studentApplicationService;
-        this.projectApplicationRequestClient = projectApplicationRequestClient;
+        this.coordinatorClient = coordinatorClient;
     }
 
     @GetMapping("/{studentCode}")
     @PreAuthorize("hasRole('student')")
     public ResponseEntity<?> getRequestsByStudentCode(@PathVariable Long studentCode) {
-        return projectApplicationRequestClient.getRequestsByStudentCode(studentCode);
+        return coordinatorClient.getRequestsByStudentCode(studentCode);
     }
 
     @PostMapping("/apply")
--- StudentService/src/main/java/co/edu/unicauca/studentservice/infra/client/CoordinatorClient.java
+++ StudentService/src/main/java/co/edu/unicauca/studentservice/infra/client/CoordinatorClient.java
@@ -3,6 +3,9 @@ package co.edu.unicauca.studentservice.infra.client;
 import co.edu.unicauca.studentservice.infra.config.FeignAuthForwardInterceptor;
 import co.edu.unicauca.studentservice.infra.dto.ProjectApplicationRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(
@@ -11,6 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
         configuration = FeignAuthForwardInterceptor.class
 )
 public interface CoordinatorClient {
+
+    @GetMapping("api/coordinator/request/student/{studentCode}")
+    ResponseEntity<?> getRequestsByStudentCode(@PathVariable Long studentCode);
 
     @PostMapping("api/coordinator/request")
     void submitProjectApplication(ProjectApplicationRequestDTO request);
